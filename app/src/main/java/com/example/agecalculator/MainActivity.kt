4365857370b9d37a4fdeebfb93404384fb82f790
ahmed salah <you@example.com>
2023-04-17 01:12:24 +0200

    the secound

--- app/src/main/java/com/example/agecalculator/MainActivity.kt
+++ app/src/main/java/com/example/agecalculator/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.agecalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.agecalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var button: Button
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        getData()
+
+    }
+
+    private fun getData(){
+        button = findViewById(R.id.datePickerBTN)
     }
 }

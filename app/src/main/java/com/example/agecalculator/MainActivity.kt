a2154327022fe1150ce34135a31800b0d4d210fc
ahmed salah <you@example.com>
2023-04-17 03:03:36 +0200

    Added Forth

--- app/src/main/java/com/example/agecalculator/MainActivity.kt
+++ app/src/main/java/com/example/agecalculator/MainActivity.kt
@@ -4,10 +4,9 @@ import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
 import com.example.agecalculator.databinding.ActivityMainBinding
 import java.text.SimpleDateFormat
+import java.time.Year
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -37,33 +36,54 @@ class MainActivity : AppCompatActivity() {
         val month = myCalendar.get(Calendar.MONTH)
         val day = myCalendar.get(Calendar.DAY_OF_MONTH)
 
-        val dpd = DatePickerDialog(this,
+        DatePickerDialog(this,
             {view, year, month, dayOfMonth ->
-
-
                 val selectedDate ="$dayOfMonth/${month+1}/$year"
                 binding.selectedDate.text = selectedDate
-
-
-                binding.dateInminutes.text = setDate(selectedDate).toString()
+                val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+                binding.dateInminutes.text = setDateInminutes(selectedDate,sdf).toString()
+                binding.dateInHours.text = setDateInHours(selectedDate,sdf).toString()
+                binding.dateInDays.text = setDateInDays(selectedDate,sdf).toString()
+                binding.dateInYear.text = setDateInYear(selectedDate,sdf,year).toString()
             },
             year,
             month,
             day,
-            )
-        dpd.show()
+            ).show()
+
 
     }
 
-    private fun setDate(selectedDate:String): Long {
-        val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+    private fun setDateInminutes(selectedDate:String,sdf:SimpleDateFormat): Long {
         val theDate = sdf.parse(selectedDate)
-
         val selectedDateInminutes = theDate.time / 6000
         val currentDateInminutes = sdf.parse(sdf.format(System.currentTimeMillis())).time / 6000
-
         val AgeInMinutes = currentDateInminutes - selectedDateInminutes
         return AgeInMinutes
+    }
+
+    private fun setDateInHours(selectedDate:String,sdf:SimpleDateFormat): Long {
+        val theDate = sdf.parse(selectedDate)
+        val selectedDateInminutes = theDate.time / (6000*60)
+        val currentDateInminutes = sdf.parse(sdf.format(System.currentTimeMillis())).time / (6000*60)
+        val AgeInHours = currentDateInminutes - selectedDateInminutes
+        return AgeInHours/10
+    }
+    private fun setDateInDays(selectedDate:String,sdf:SimpleDateFormat): Long {
+        val theDate = sdf.parse(selectedDate)
+        val selectedDateInminutes = theDate.time / (6000*60*24)
+        val currentDateInminutes = sdf.parse(sdf.format(System.currentTimeMillis())).time / (6000*60*24)
+        val AgeInDays = currentDateInminutes - selectedDateInminutes
+        return AgeInDays/10
+    }
+
+    private fun setDateInYear(selectedDate:String,sdf:SimpleDateFormat,year:Int): Int {
+        val theDate = sdf.parse(selectedDate)
+        val selectedDateInminutes = year
+
+        val currentDateInminutes = Calendar.getInstance().get(Calendar.YEAR)
+        val AgeInYear = currentDateInminutes - selectedDateInminutes
+        return AgeInYear
     }
 
 }
